b18b4b2843ab8541cc18d19920ead9e3ef1dc231
Jaime0506 <jaimemejia1003@gmail.com>
2024-05-09 14:26:53 -0500

    Jump Done

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,27 +6,41 @@ public class PlayerMove : MonoBehaviour
 {
     public CharacterController controller;
     public Animator animator;
-    
+
+    public float horizontal;
+    public float vertical;
+
     public float speed = 5f;
     public float turnSmoothTime = 0.1f;
     public float turnSmoothVelocity;
     public Transform cam;
 
+    public float jumpForce = 5f; // Fuerza del salto
+
+    private bool isJumping = false; // Bandera para controlar el salto
+
     // Start is called before the first frame update
     void Start()
     {
+        controller = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertial = Input.GetAxis("Vertical");
+        MovePlayer();
+    }
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertial).normalized;
+    void MovePlayer()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
-        if (direction.magnitude >= 0.1f) {
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
             float targetAngule = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angule = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngule, ref turnSmoothVelocity, turnSmoothTime);
 
@@ -36,8 +50,36 @@ public class PlayerMove : MonoBehaviour
             controller.Move(speed * Time.deltaTime * moveDiretion);
 
             animator.SetBool("walking", true);
-        } else {
+        }
+        else
+        {
             animator.SetBool("walking", false);
         }
+
+        if (controller.isGrounded)
+        {
+            // Salto cuando se presiona el botón y no está saltando
+            if (Input.GetButton("Jump") && !isJumping)
+            {
+                isJumping = true;
+                StartCoroutine(Jump());
+            }
+        } 
+    }
+
+    IEnumerator Jump()
+    {
+        float timer = 0f;
+
+        while (timer < 0.5f) // Tiempo de salto
+        {
+            float jumpHeight = Mathf.Sqrt(2 * jumpForce * -Physics.gravity.y);
+            controller.Move(Vector3.up * jumpHeight * Time.deltaTime);
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        isJumping = false;
     }
 }
