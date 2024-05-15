c9ba778e4cbf619fc0f626a89d23f48ba6cb73d3
Jaime0506 <jaimemejia1003@gmail.com>
2024-05-15 14:38:05 -0500

    Add interactivity with chairs

---
+++ Assets/Scripts/TakeObject.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeObject : MonoBehaviour
+{
+    public GameObject handPoint;
+    private GameObject pickedObject = null;
+
+    private void Update() {
+        // Flujo grama
+        if(pickedObject != null) {
+            if (Input.GetKey("r")) {
+                pickedObject.GetComponent<Rigidbody>().useGravity = true;
+                pickedObject.GetComponent<Rigidbody>().isKinematic = false;
+
+                pickedObject.gameObject.transform.SetParent(null);
+                pickedObject = null;
+            }
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Object"))
+        {
+            if (Input.GetKey("e") && pickedObject == null)
+            {
+                other.GetComponent<Rigidbody>().useGravity = false;
+                other.GetComponent<Rigidbody>().isKinematic = true;
+
+                other.transform.position = handPoint.transform.position;
+                other.gameObject.transform.SetParent(handPoint.gameObject.transform);
+                pickedObject = other.gameObject;
+            }
+        }
+    }
+}
